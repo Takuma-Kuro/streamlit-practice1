aac4b9484f9e6759bcd44d92436712c9a12cce9d
黒川卓真 <skijump120@gmail.com>
2021-08-02 14:56:17 +0900

    1st commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import time
+
+st.title('Streamlit 超入門')
+
+st.write('プログレスバー')
+'Start!!'
+
+# text = st.sidebar.text_input('あなたの趣味を教えてください')
+# condition = st.sidebar.slider('あなたの調子は？', 0, 100, 50)
+
+# 'あなたの趣味は', text
+# 'コンディション', condition, '%'
+
+# if st.checkbox('Show Image'):
+#     img = Image.open('sample.png')
+#     st.image(img,caption='サンプル', use_column_width=True)
+
